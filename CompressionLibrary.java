b9364f7e36ea41572063cb641dc4cfa516d41e28
RDragoo <47581540+RDragoo@users.noreply.github.com>
2019-05-30 15:48:04 -0600

    Add files via upload
    Added Import

--- CompressionLibrary.java
+++ CompressionLibrary.java
@@ -1,8 +1,13 @@
 package MasterLib;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import javax.imageio.ImageIO;
 
 public class CompressionLibrary {
 	
@@ -29,16 +34,46 @@ public class CompressionLibrary {
 		return tempPath;
 	}
 	
-	public static void deleteTempFolder(Path toDelete) {
+	public static boolean deleteTempFolder(Path toDelete) {
 		
+		boolean isComplete = false;
 		try {
 			if (toDelete != null) {
 				toDelete.toFile().deleteOnExit(); //It's more efficient to delete the folder once the program closes.
+				isComplete = true;
 			}
 		} catch (SecurityException c) {
 			c.printStackTrace();
+			isComplete = false;
 		}
+		
+		return isComplete;
 	}
 	
+	public static boolean importImgToJPG(Path readPath, Path tempPath) {
+		
+		boolean isComplete = false;
+		try {
+			File input = readPath.toFile();
+			BufferedImage inkjet = ImageIO.read(input);
+			BufferedImage copied = new BufferedImage(inkjet.getWidth(), inkjet.getHeight(), BufferedImage.TYPE_INT_RGB);
+			copied.createGraphics().drawImage(inkjet, 0, 0, Color.WHITE, null);
+			
+			String tempNamer = tempPath.toString() + "tempForCompression.jpg";
+			Path tempPather = Paths.get(tempNamer);
+			File output = tempPather.toFile();
+			
+			ImageIO.write(copied, "jpg", output);
+			
+			isComplete = true;
+		}
+		catch (IOException a) {
+			a.printStackTrace();
+			isComplete = false;
+		}
+		
+		return isComplete;
+	}
+			
 	
 }
